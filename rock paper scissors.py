9cd1f824f39930f3e1a0d356f16eb964efd90ce1
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-08-17 16:29:29 +0200

    Add files via upload

---
+++ rock paper scissors.py
@@ -0,0 +1,16 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+while True:
+  user_input = input('Type Rock/Paper/Scissors or Q to quit: ').lower()
+  if user_input == "q":
+    break
+
+  if user_input not in ["rock", "paper", "scissors"]
+    continue
+
+  random_number = random.randint(0, 2)
+
+print("Goodbye!")
